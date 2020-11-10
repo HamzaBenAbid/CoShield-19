136c573e839d2c11a95be5ba60def5dc362cc5b2
HamzaBenAbid <hamzabozambo@gmail.com>
2020-11-10 12:40:24 +0100

    ajouter paage signe up

--- app/src/main/java/com/example/coshiled19/Login.java
+++ app/src/main/java/com/example/coshiled19/Login.java
@@ -2,15 +2,27 @@ package com.example.coshiled19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
-
+    Button callsignup ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN, WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
         setContentView(R.layout.activity_login);
+        callsignup =findViewById(R.id.signup_button);
+        callsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(Login.this,Signup2.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
