a28cbc13e838209cc4b6a22fc6dd82122d2a9be6
HamzaBenAbid <hamzabozambo@gmail.com>
2020-11-13 15:33:51 +0100

    scrollview

--- app/src/main/java/com/example/coshiled19/Home.java
+++ app/src/main/java/com/example/coshiled19/Home.java
@@ -4,19 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-
-public class Signup2 extends AppCompatActivity {
-
+public class Home extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup2);
-
-
+        setContentView(R.layout.activity_home);
     }
-
-
-
-
 }
