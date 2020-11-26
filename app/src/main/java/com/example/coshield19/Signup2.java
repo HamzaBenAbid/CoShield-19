56edaf522198014179519f3a9b843f99d2061b2a
HamzaBenAbid <hamzabozambo@gmail.com>
2020-11-26 15:38:28 +0100

    already have ana acout navigation

--- app/src/main/java/com/example/coshield19/Signup2.java
+++ app/src/main/java/com/example/coshield19/Signup2.java
@@ -2,18 +2,29 @@ package com.example.coshield19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.coshiled19.R;
 
 
 public class Signup2 extends AppCompatActivity {
 
-
+    Button button ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup2);
+        button =findViewById(R.id.dont_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(Signup2.this,Login.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
