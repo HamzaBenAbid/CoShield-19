75d364e3d57266bd26f477d11f46c3cfe039c731
hamza ben abid <hamzabozambo@gmail.com>
2020-12-22 22:35:08 +0100

    Validate Registration  - Form validation

--- app/src/main/java/com/example/coshield19/Signup2.java
+++ app/src/main/java/com/example/coshield19/Signup2.java
@@ -15,6 +15,7 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class Signup2 extends AppCompatActivity {
     TextInputLayout a , b , c , d ;
+
     Button button  , button2 ;
     FirebaseDatabase root;
     DatabaseReference reference;
@@ -32,18 +33,20 @@ public class Signup2 extends AppCompatActivity {
             }
         });
 
-
         a= findViewById(R.id.reg_username);
         b= findViewById(R.id.reg_email);
         c= findViewById(R.id.reg_password);
        d= findViewById(R.id.reg_phone_number);
+
         button2 =findViewById(R.id.go_btn);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
              root = FirebaseDatabase.getInstance();
              reference = root.getReference("users");
-
+if (!validateName() | !validateEmail() | !validatepassword() | !validatephone()){
+return;
+}
              //get all the values
                 String name = a.getEditText().getText().toString();
                 String email = b.getEditText().getText().toString();
@@ -55,8 +58,70 @@ public class Signup2 extends AppCompatActivity {
         });
 
     }
-
-
-
+    private  Boolean validateName(){
+        String name = a.getEditText().getText().toString();
+        if (name.isEmpty()){
+            a.setError("Field cannot be empty");
+            return  false;
+        }else if (name.length()>=15){
+            a.setError("Username too Long");
+            return  false;
+        }else {
+            a.setError(null);
+            a.setErrorEnabled(false);
+            return true;
+        }
+    }
+    private  Boolean validateEmail(){
+        String email = b.getEditText().getText().toString();
+        String em ="[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+        if (email.isEmpty()){
+            b.setError("Field cannot be empty");
+            return  false;
+        }else if (!email.matches(em)){
+            b.setError("Invalid email address");
+            return  false;
+        }else {
+           b.setError(null);
+            b.setErrorEnabled(false);
+            return true;
+        }
+    }
+    private  Boolean validatephone(){
+        String phoneNo = d.getEditText().getText().toString();
+        if (phoneNo.isEmpty()){
+            d.setError("Field cannot be empty");
+            return  false;
+        }else if (phoneNo.length()!=8){
+            a.setError("invalid phone number ");
+            return  false;}
+        else {
+            d.setError(null);
+            d.setErrorEnabled(false);
+            return true;
+        }
+    }
+    private  Boolean validatepassword(){
+        String password = c.getEditText().getText().toString();
+        String pw ="^"+
+              //  "(?=.*[0-9])"+
+             //   "(?=.*[a-z])"+
+                "(?=.*[A-Z])"+
+                "(?=.*[@#$%^&+=])"+
+                "(?=\\S+$)"+
+                ".{4,}"+
+                "$";
+        if (password.isEmpty()){
+            c.setError("Field cannot be empty");
+            return  false;
+        }else if (!password.matches(pw)){
+            c.setError("Invalid password");
+            return  false;
+        }else {
+            c.setError(null);
+           c.setErrorEnabled(false);
+            return true;
+        }
+    }
 
 }
