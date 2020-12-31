474bb0951661ad4930adb8a70e030de371a59c03
Ghayth Fdaoui <Ghaythfd@gmail.com>
2020-12-31 09:28:13 +0100

    Java for Take test2 page not working properly

--- app/src/main/java/com/dsi32g1/coshield19/taketest.java
+++ app/src/main/java/com/dsi32g1/coshield19/taketest.java
@@ -2,17 +2,60 @@ package com.dsi32g1.coshield19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+import android.widget.CheckBox;
+import android.widget.Spinner;
+
 
 public class taketest extends AppCompatActivity {
 
+ /*
     @Override
     protected void onCreate(Bundle savedInstanceState) {
        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_taketest2);
-       // Spinner dropdown = findViewById(R.id.age_spinner);
-       // String[] items = new String[]{"0-10","11-20","21-30","31-40","41-50","50+"};
-        //ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.age_spinner, items);
-        //dropdown.setAdapter(adapter);*/
+         Spinner dropdown = findViewById(R.id.age_spinner);
+           String[] items = new String[]{"0-10","11-20","21-30","31-40","41-50","50+"};
+
+           ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout., items);
+           dropdown.setAdapter(adapter);
+            
+
+
+
+        public void onCheckboxClicked(View view ) {
+            int score=100 ;
+            boolean checked = ((CheckBox) view).isChecked();
+
+            
+            switch(view.getId()) {
+                    case R.id.fever:
+                        if (checked) 
+                            score=score-10; 
+                    case R.id.sore_throat:
+                        if (checked)
+                            score=score-10;
+                    case R.id.smell_taste:
+                        if (checked)
+                            score=score-10;
+                    case R.id.tiredness:
+                        if (checked)
+                            score=score-10;
+
+
+                    else
+
+                    break;
+
+            }
+        }
     }
 }
+
+      */
